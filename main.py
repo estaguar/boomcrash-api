449ca85d2efa73d111b3bddcce1aedd6a1d38714
estaguar <esteban.agcr@gmail.com>
2025-05-09 16:24:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -82,5 +82,6 @@ def boomcrash():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
 
